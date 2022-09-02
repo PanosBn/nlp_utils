7a3744b5a8a5ad30ae51a6f2afd9f19fc583ae08
Παναγιωτης Μπανος <panos@wlan-185117.nbw.tue.nl>
2022-09-02 14:33:12 +0200

    remove comments

--- utils/spacy_conll.py
+++ utils/spacy_conll.py
@@ -36,43 +36,26 @@ class ConllConverter():
         docstart = "-DOCSTART-"
         empty = "O"
         for index, doc in enumerate(tqdm(docs)):
-            # self.conll_data += str("-DOCSTART-") + "\t" + str("O")
-            # self.conll_data += "\t".join((docstart,empty))
-            # self.conll_data += "\n"
-            # self.conll_data += "\n"
+
             self.result.append("\t".join((docstart,empty)))
             self.result.append("\n")
             self.result.append("\n")
             for sent in doc.sents:
-                # print(sent)
-                # print(sent)
-                # print(sent.ents)
                 for tok in sent:
                         label = tok.ent_iob_
                         if tok.ent_iob_ != "O":
-                            # print(tok.ent_iob_)
                             label += '-' + tok.ent_type_
-                        # print(tok, label, sep="\t")
                         if not str(tok).isspace():
-                            # self.conll_data += str("sentence ") +str(index+1)  +"\t" + str(tok) + "\t" + str(label)
+
                             self.result.append("sentence " +str(index+1)  +"\t" + str(tok) + "\t" + label)
                             self.result.append("\n")
 
-                            # self.conll_data += "\n"
-                            
-                            # print(temp_data)
-                            # print(str(label))
-                # self.conll_data += "\n"
                 self.result.append("\n")
 
 
         return self
 
     def write(self, output_path: str):
-        # fd = open(output_path, "w",encoding="utf-8")
-        # fd.write(self.result)
-        # fd.close()
-        # print("CoNLL done")
 
         with open(output_path, "w",encoding="utf-8") as outfile:
             outfile.write("".join(self.result))
